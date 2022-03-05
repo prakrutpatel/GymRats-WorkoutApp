cef786912c97f83f3f4527aea09566d3af17d9a8
prakrut <ppatel@eckerd.edu>
2022-03-05 09:59:49 -0500

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ec_fitness/main.dart';
+import 'file:///C:/Users/cnms6/StudioProjects/EC_Fitness/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
