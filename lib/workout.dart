0478c35169d2acbc9e2475ed9a47597a1631a3ae
Micah Lessnick <micahlessnick@gmail.com>
2022-03-17 10:31:20 -0400

    added ability to move elements of exercise list and swipe them away (to delete elements)

--- lib/workout.dart
+++ lib/workout.dart
@@ -1,67 +1,113 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const Workout());
+void main() => runApp(const WorkoutTest());
 
-class Workout extends StatelessWidget {
-  const Workout({Key? key}) : super(key: key);
+class WorkoutTest extends StatelessWidget {
+  const WorkoutTest({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Workout List';
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
+      home: ExerciseList(),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+class ExerciseList extends StatefulWidget {
+  const ExerciseList({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<ExerciseList> createState() => _ExerciseListState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  List<int> top = <int>[];
-  List<int> bottom = <int>[0];
+class _ExerciseListState extends State<ExerciseList> {
+  List<Exercise> bottom = <Exercise>[];
+
+  Future createAlertDialog(BuildContext context) {
+    TextEditingController exNameCont = TextEditingController();
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Enter Exercise:"),
+            content: TextField(
+              controller: exNameCont,
+            ),
+            actions: [
+              MaterialButton(
+                elevation: 5.0,
+                child: const Text("Submit"),
+                onPressed: () {
+                  Navigator.of(context).pop(exNameCont.text.toString());
+                },
+              )
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    const Key centerKey = ValueKey<String>('bottom-sliver-list');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add exercise'),
         leading: IconButton(
           icon: const Icon(Icons.add),
           onPressed: () {
-            setState(() {
-              top.add(-top.length - 1);
-              bottom.add(bottom.length);
+            createAlertDialog(context).then((onValue) {
+              Exercise t = Exercise(onValue, "[empty]", 0, 0, 0);
+              setState(() {
+                bottom.add(t);
+              });
             });
           },
         ),
       ),
-      body: CustomScrollView(
-        center: centerKey,
-        slivers: <Widget>[
-          SliverList(
-            key: centerKey,
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.blue[200],
-                  height: 100,
-                  child: Text('Item: ${bottom[index]}'),
-                );
+      body: ReorderableListView.builder(
+          itemCount: bottom.length,
+          itemBuilder: (context, index) {
+            final item = bottom[index];
+            return Dismissible(
+              key: Key('$index'),
+              onDismissed: (direction) {
+                // Remove the item from the data source.
+                setState(() {
+                  bottom.removeAt(index);
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(item.name + ' dismissed')));
               },
-              childCount: bottom.length,
-            ),
-          ),
-        ],
-      ),
+              // Show a red background as the item is swiped away.
+              background: Container(color: Colors.red),
+              child: ListTile(
+                  title: Text("Name: " + item.name + "\nType: " + item.type),
+                  trailing: const Icon(Icons.drag_handle)),
+            );
+          },
+          onReorder: (int oldIndex, int newIndex) {
+            setState(() {
+              if (oldIndex < newIndex) {
+                newIndex -= 1;
+              }
+              final Exercise item = bottom.removeAt(oldIndex);
+              bottom.insert(newIndex, item);
+            });
+          }),
     );
   }
+}
+
+class Exercise {
+  //constructor to act like tuple
+  String name;
+  String type;
+  int reps;
+  int sets;
+  int maxWeight;
+
+  Exercise(this.name, this.type, this.reps, this.sets, this.maxWeight);
 }
