aa72cd15a2b8db2ffea2ff733b2efb93d7dd2e9e
Micah Lessnick <micahlessnick@gmail.com>
2022-03-29 09:13:31 -0400

    Added duration field input

--- lib/workout.dart
+++ lib/workout.dart
@@ -1,6 +1,7 @@
 //Written by Micah Lessnick
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math' as math;
 
 class ExerciseList extends StatefulWidget {
   const ExerciseList({Key? key}) : super(key: key);
@@ -34,6 +35,7 @@ class _ExerciseListState extends State<ExerciseList>
     TextEditingController exTypeCont = TextEditingController();
     TextEditingController repsCont = TextEditingController();
     TextEditingController setsCont = TextEditingController();
+    TextEditingController timeCont = TextEditingController();
 
     return showDialog(
         context: context,
@@ -62,6 +64,17 @@ class _ExerciseListState extends State<ExerciseList>
                   keyboardType: TextInputType.number,
                   inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                   decoration: const InputDecoration(hintText: "sets"),
+                ),
+                TextFormField(
+                  controller: timeCont,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: false),
+                  decoration: const InputDecoration(
+                    hintText: '00:00:00',
+                  ),
+                  inputFormatters: <TextInputFormatter>[
+                    TimeTextInputFormatter() // This input formatter will do the job
+                  ],
                 )
               ],
             ),
@@ -70,12 +83,23 @@ class _ExerciseListState extends State<ExerciseList>
                 elevation: 5.0,
                 child: const Text("Submit"),
                 onPressed: () {
+                  int durInSec = 0; //duration in seconds
+                  String durTemp = timeCont.text;
+                  List<String> splitDur = timeCont.text
+                      .toString()
+                      .split(':'); //time segments stored into array
+                  durInSec += (int.parse(splitDur[0]) * 3600) +
+                      (int.parse(splitDur[1]) * 60) +
+                      (int.parse(splitDur[
+                          2])); //convert array into actual time (in seconds)
                   Navigator.of(context).pop(Exercise(
                       exNameCont.text.toString(),
                       exTypeCont.text.toString(),
                       int.parse(repsCont.text),
                       int.parse(setsCont.text),
                       0,
+                      durInSec,
+                      durTemp,
                       _counter));
                 },
               )
@@ -125,7 +149,9 @@ class _ExerciseListState extends State<ExerciseList>
                       "\nReps: " +
                       item.reps.toString() +
                       "\nSets: " +
-                      item.sets.toString()),
+                      item.sets.toString() +
+                      "\nDuration: " +
+                      item.durDisp),
                   trailing: const Icon(Icons.drag_handle)),
             );
           },
@@ -142,6 +168,120 @@ class _ExerciseListState extends State<ExerciseList>
   }
 }
 
+class TimeTextInputFormatter extends TextInputFormatter {
+  //input formatter for making a time field input
+  RegExp _exp = RegExp(r'^[0-9:]+$');
+  TimeTextInputFormatter() {
+    _exp = RegExp(r'^[0-9:]+$');
+  }
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    if (_exp.hasMatch(newValue.text)) {
+      TextSelection newSelection = newValue.selection;
+
+      String value = newValue.text;
+      String newText;
+
+      String leftChunk = '';
+      String rightChunk = '';
+
+      if (value.length >= 8) {
+        if (value.substring(0, 7) == '00:00:0') {
+          leftChunk = '00:00:';
+          rightChunk = value.substring(leftChunk.length + 1, value.length);
+        } else if (value.substring(0, 6) == '00:00:') {
+          leftChunk = '00:0';
+          rightChunk = value.substring(6, 7) + ":" + value.substring(7);
+        } else if (value.substring(0, 4) == '00:0') {
+          leftChunk = '00:';
+          rightChunk = value.substring(4, 5) +
+              value.substring(6, 7) +
+              ":" +
+              value.substring(7);
+        } else if (value.substring(0, 3) == '00:') {
+          leftChunk = '0';
+          rightChunk = value.substring(3, 4) +
+              ":" +
+              value.substring(4, 5) +
+              value.substring(6, 7) +
+              ":" +
+              value.substring(7, 8) +
+              value.substring(8);
+        } else {
+          leftChunk = '';
+          rightChunk = value.substring(1, 2) +
+              value.substring(3, 4) +
+              ":" +
+              value.substring(4, 5) +
+              value.substring(6, 7) +
+              ":" +
+              value.substring(7);
+        }
+      } else if (value.length == 7) {
+        if (value.substring(0, 7) == '00:00:0') {
+          leftChunk = '';
+          rightChunk = '';
+        } else if (value.substring(0, 6) == '00:00:') {
+          leftChunk = '00:00:0';
+          rightChunk = value.substring(6, 7);
+        } else if (value.substring(0, 1) == '0') {
+          leftChunk = '00:';
+          rightChunk = value.substring(1, 2) +
+              value.substring(3, 4) +
+              ":" +
+              value.substring(4, 5) +
+              value.substring(6, 7);
+        } else {
+          leftChunk = '';
+          rightChunk = value.substring(1, 2) +
+              value.substring(3, 4) +
+              ":" +
+              value.substring(4, 5) +
+              value.substring(6, 7) +
+              ":" +
+              value.substring(7);
+        }
+      } else {
+        leftChunk = '00:00:0';
+        rightChunk = value;
+      }
+
+      if (oldValue.text.isNotEmpty && oldValue.text.substring(0, 1) != '0') {
+        if (value.length > 7) {
+          return oldValue;
+        } else {
+          leftChunk = '0';
+          rightChunk = value.substring(0, 1) +
+              ":" +
+              value.substring(1, 2) +
+              value.substring(3, 4) +
+              ":" +
+              value.substring(4, 5) +
+              value.substring(6, 7);
+        }
+      }
+
+      newText = leftChunk + rightChunk;
+
+      newSelection = newValue.selection.copyWith(
+        baseOffset: math.min(newText.length, newText.length),
+        extentOffset: math.min(newText.length, newText.length),
+      );
+
+      return TextEditingValue(
+        text: newText,
+        selection: newSelection,
+        composing: TextRange.empty,
+      );
+    }
+    return oldValue;
+  }
+}
+
 class Exercise {
   //constructor to act like tuple
   String name;
@@ -149,8 +289,10 @@ class Exercise {
   int reps;
   int sets;
   int maxWeight;
+  int dur;
+  String durDisp; //allows real time to be stored and formatted to be displayed
   int uid;
 
-  Exercise(
-      this.name, this.type, this.reps, this.sets, this.maxWeight, this.uid);
+  Exercise(this.name, this.type, this.reps, this.sets, this.maxWeight, this.dur,
+      this.durDisp, this.uid);
 }
